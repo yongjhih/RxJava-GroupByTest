d6db720d3cb31223b66ae9e03173ac45a6099222
Andrew Chen <yongjhih@gmail.com>
2015-03-24 23:53:03 +0800

    rename

--- src/main/java/com/github/yongjhih/Main.java
+++ src/main/java/com/github/yongjhih/Main.java
@@ -7,7 +7,7 @@ import rx.observables.*;
 import rx.util.*;
 import rx.internal.operators.*;
 
-public class GroupByTest {
+public class Main {
     public static void main(String[] args) {
         /* original */
         Observable.range(1, 10)
